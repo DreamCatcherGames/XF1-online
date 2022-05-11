4e1730dae0496ddb28f0f5771fddf2b6618d2c97
Kevin Zeledón <magneticok@gmail.com>
2022-05-10 18:55:22 -0600

    Fecha de inicio y finalizacion

--- src/app/pages/admin-page/components/campeonato-info/campeonato-info.component.ts
+++ src/app/pages/admin-page/components/campeonato-info/campeonato-info.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CampeonatoInfoComponent implements OnInit {
 
-  myDatePicker:any;
+  startDatetime:any;
+  endDatetime:any;
 
   constructor() { }
 
