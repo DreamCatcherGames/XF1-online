54a9a7c00559bdc469d730b87052f3169cad3eaa
Kevin Zeledón <magneticok@gmail.com>
2022-05-22 17:22:32 -0600

    Mas pruebas

--- src/app/pages/admin-page/historico-campeonatos/historico-campeonatos.component.spec.ts
+++ src/app/pages/admin-page/historico-campeonatos/historico-campeonatos.component.spec.ts
@@ -1,4 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { CampeonatoService } from 'src/app/service/campeonato.service';
 
 import { HistoricoCampeonatosComponent } from './historico-campeonatos.component';
 
@@ -6,9 +8,22 @@ describe('HistoricoCampeonatosComponent', () => {
   let component: HistoricoCampeonatosComponent;
   let fixture: ComponentFixture<HistoricoCampeonatosComponent>;
 
+  // Fakes
+  let fakeCampeonatoService: CampeonatoService;
+  let fakeRouter: Router;
+
+
   beforeEach(async () => {
+
+    fakeRouter = jasmine.createSpyObj<Router>('Router', ['navigateByUrl']);
+    fakeCampeonatoService = jasmine.createSpyObj<CampeonatoService>('CampeonatoService', ['getAllButCurrent']);
+
     await TestBed.configureTestingModule({
-      declarations: [ HistoricoCampeonatosComponent ]
+      declarations: [ HistoricoCampeonatosComponent ],
+      providers: [
+        {provide:CampeonatoService, useValue: fakeCampeonatoService},
+        {provide:Router, useValue: fakeRouter}
+      ]
     })
     .compileComponents();
   });
@@ -21,5 +36,11 @@ describe('HistoricoCampeonatosComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect when clicking go to inicial', ()=>{
+    component.goToInicial();
+
+    expect(fakeRouter.navigateByUrl).toHaveBeenCalledWith('/admin/campeonato-actual');
   });
 });
