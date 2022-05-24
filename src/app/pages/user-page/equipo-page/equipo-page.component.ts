7bddf155c02b2daac9c5726984c45808bda38e7d
Kevin Zeledón <magneticok@gmail.com>
2022-05-23 22:12:25 -0600

    Toggling

--- src/app/pages/user-page/equipo-page/equipo-page.component.ts
+++ src/app/pages/user-page/equipo-page/equipo-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-equipo-page',
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipoPageComponent implements OnInit {
 
-  constructor() { }
+  selectedFrame:number;
+  showingMarket:boolean = false;
+  marketType:string = '';
+
+  constructor(private renderer:Renderer2) { 
+    this.renderer.listen('window', 'click', e=>{
+      console.log(e.target)
+      if(
+          !(e.target.className.includes("toggleMarket"))
+        ){
+          this.showingMarket = false;
+          this.selectedFrame = 0;
+        }
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  setActiveFrame(frameNumber:number){
+    this.marketType='piloto';
+    this.selectedFrame = frameNumber;
+    this.showingMarket = true;
+  }
+
+  setActiveFrameEscuderia(frameNumber:number){
+    this.marketType = 'escuderia';
+    this.selectedFrame = frameNumber;
+    this.showingMarket = true;
   }
 
 }
