2588bfca71256a45ccea8eea674d47f490d5fa18
Kevin Zeledón <magneticok@gmail.com>
2022-06-06 23:33:10 -0600

    Dummy profile

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Equipo } from '../models/equipo';
 import { Perfil } from '../models/perfil';
 import { PerfilUsuario } from '../models/perfilUsuario';
 import { RestService } from './rest.service';
@@ -10,7 +11,22 @@ import { RestService } from './rest.service';
 export class AuthService {
 
   public perfil : Perfil;
-  public perfilUsuario : PerfilUsuario;
+  public perfilUsuario : PerfilUsuario={
+    Teams:[
+      new Equipo(),
+      new Equipo()
+    ],
+    Username:'',
+    Country:'',
+    First_Name:'',
+    Last_Name:'',
+    Email:'',
+    Money:0,
+    Encrypted_Password:'',
+    Salt:'',
+    Token:'',
+    Active:true
+  };
 
   constructor(
     private restService: RestService,
